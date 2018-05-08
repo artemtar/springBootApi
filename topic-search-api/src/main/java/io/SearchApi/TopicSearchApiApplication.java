f509b040718f421251fb11579c1d4dc9ba4da92a
artemtar <artemtarasov89@gmail.com>
2018-05-08 19:50:46 -0400

    topic search api

---
+++ topic-search-api/src/main/java/io/SearchApi/TopicSearchApiApplication.java
@@ -0,0 +1,12 @@
+package io.SearchApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopicSearchApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopicSearchApiApplication.class, args);
+	}
+}
